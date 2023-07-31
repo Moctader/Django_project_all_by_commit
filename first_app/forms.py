334b64ba8d0cb78f113074efe9b508ceda258dcf
Md Golam Moctader <golam.moctader@gmail.com>
2023-07-31 20:47:52 +0300

    Not getting user values in backend it's a Bug

--- first_app/forms.py
+++ first_app/forms.py
@@ -9,3 +9,7 @@ class contactForm(forms.Form):
     check=forms.BooleanField()
     bithday=forms.DateField()
     appointment=forms.DateTimeField()
+    CHOICES=[('s', 'small'), ('M', 'medium'),('L','Large')]
+    choice=forms.ChoiceField(choices=CHOICES)
+    MEAL=[('p','peparoni'),('M','Mashroom'),('B', 'Beef')]
+    pizza=forms.MultipleChoiceField(choices=MEAL)
