d59ac7d01d247d99351fc5bdf876a97539fdf702
Md Golam Moctader <golam.moctader@gmail.com>
2023-08-01 01:28:47 +0300

    Django form attributes and widget

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,23 +1,18 @@
 from django import forms
 
 class contactForm(forms.Form):
-    name=forms.CharField(label="User Name")
-    file=forms.FileField()
-    
-    
-    
-    
-    
-    
-    
-    # email=forms.EmailField(label="User Email")
-    # age=forms.IntegerField()
+    name=forms.CharField(label="Full Name:", help_text="Enter your full name",
+    required=False, widget=forms.Textarea (attrs={'id' : 'text_area', 'placeholder' : 'Enter your name'}))
+   
+   
+    email=forms.EmailField(label="User Email", required=False)
     # weight=forms.FloatField()
     # balance=forms.DecimalField()
-    # check=forms.BooleanField()
-    # bithday=forms.DateField()
-    # appointment=forms.DateTimeField()
-    # CHOICES=[('s', 'small'), ('M', 'medium'),('L','Large')]
-    # choice=forms.ChoiceField(choices=CHOICES)
-    # MEAL=[('p','peparoni'),('M','Mashroom'),('B', 'Beef')]
-    # pizza=forms.MultipleChoiceField(choices=MEAL)
+    age=forms.CharField(widget=forms.NumberInput, required=False)
+    check=forms.BooleanField()
+    bithday=forms.CharField(widget=forms.DateInput(attrs={'type' : 'date'}))
+    appointment=forms.CharField(widget=forms.DateInput(attrs={'type' : 'datetime-local'}))
+    CHOICES=[('small', 'small'), ('Medium', 'medium'),('Large','Large')]
+    choice=forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    MEAL=[('p','peparoni'),('M','Mashroom'),('B', 'Beef')]
+    pizza=forms.MultipleChoiceField(choices=MEAL, widget=forms.CheckboxSelectMultiple)
